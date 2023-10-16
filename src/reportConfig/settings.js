85f51ddccd1fa0884594db318f58b7f8f9e05955
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2023-10-16 12:39:20 -0500

    ReportConfig deleted

--- src/reportConfig/settings.js
+++
@@ -1,5 +0,0 @@
-// YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
-export const mongoConfig = {
-    serverUrl: 'mongodb+srv://.....',
-    database: 'FRAPReportedFraud_Incidents'
-  };
