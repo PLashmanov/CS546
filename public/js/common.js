faf45ec3550764eacc6ea51e93be22c6b2ec1694
Pavel Lashmanov <plashmanov@gmail.com>
2023-10-31 23:18:24 -0400

    html and css cleanup, accesibility, js cleanup, searchbar

--- public/js/common.js
+++ public/js/common.js
@@ -16,3 +16,58 @@ $(function(){
 
     });
 });
+
+$(document).ready(function() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.firstName) {
+        $('#userName').text(user.firstName);
+    }
+});
+//Navigation bar
+
+ $(function(){
+    $("#nav-placeholder").load("nav.html");
+ });
+
+ //report functions date mandatory and 1 other mandatory field,needs more validations for email and other stuff.
+
+function validateReport(){
+    const dateField = document.getElementById('FraudDate');
+    const restFields = ['firstName', 'lastName', 'email', 'website', 'phoneNumber'];
+
+    if(dateField.value.trim() === ''){
+        alert('Date is required.');
+        return false;
+    }
+
+    const oneFieldCompleted = restFields.some(fieldName => {
+        const field = document.getElementById(fieldName);
+        return field.value.trim() !== '';
+    });
+    if(!oneFieldCompleted) {
+        alert('At least one field is required.');
+        return false;
+    }
+    return true;
+}
+
+//Log out button
+
+document.addEventListener('DOMContentLoaded', function () {
+    // The following code will execute once the DOM is fully loaded
+
+    const logout = document.getElementById('logout');
+
+    if (logout) { // Check if the element is found
+        logout.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.location.href = 'index.html';
+            alert('Thank you for your contributions. You have been logged out.');
+        });
+    } else {
+        console.error('Element with id "logout" not found in the DOM.');
+    }
+});
+
+
+
---
+++ public/js/lookup.js
@@ -0,0 +1,17 @@
+/* we will need to add the server side and the ajax requests also routes and validations*/
+document.addEventListener('DOMContentLoaded', function () {
+    const searchBar = document.getElementById('search-bar');
+    const searchCriteria = document.getElementById('search-criteria');
+    //const searchQuery = document.getElementById('search-query');
+    //const searchResults = document.getElementById('search-results');
+
+    searchBar.addEventListener('submit', function (event) {
+        event.preventDefault();
+        
+        const selectedCriteria = searchCriteria.value;
+        const query = searchQuery.value;
+    //return the results
+        
+        
+    });
+});
