a40344df11ff581b7dd0ffd1edda356257ef94fd
Pavel Lashmanov <114600186+PLashmanov@users.noreply.github.com>
2023-11-07 17:01:00 -0500

    look up polish

--- public/js/lookup.js
+++ public/js/lookup.js
@@ -1,10 +1,8 @@
-/*import * as validations from '../src/validations/Validations.js'
- we will need to add the server side and the ajax requests also routes and validations
+
 
 document.addEventListener('DOMContentLoaded', function () {
     const searchBar = document.getElementById('search-bar');
-    const searchCriteria = document.getElementById('search-criteria');
-    //const searchQuery = document.getElementById('search-query');
+   
     //const searchResults = document.getElementById('search-results');
 
     searchBar.addEventListener('submit', function (event) {
@@ -27,14 +25,48 @@ const emailLook = document.getElementById('look-email').value;
 const phoneLook = document.getElementById('look-phone').value;
 
 
-/*function searchInputValidation() {
+export function searchValidation() {
+    const searchCriteria = document.getElementById('search-criteria');
+    const searchQuery = document.getElementById('search-query');
 
     if(searchCriteria === 'phone'){
-        const phonePatt = validations.validatePhoneNumber();
-        if(!phonePatt.test(searchQuery)){
+        const phoneP =  /^\d{10}$/;
+        if(!phoneP.test(searchQuery)){
             errorMessage.textContent = 'Invalid Phone number format';
             return false;
         }
+    } else if (searchCriteria === 'email') {
+        const emailP = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if(!emailP.test(searchQuery)) {
+            errorMessage.textContent = 'Invalid email format';
+            return false;
+        }
+    } else if (searchCriteria === 'name') {
+        const nameP = /^[A-Za-z\s]+$/;
+        if(!nameP.test(searchQuery)){
+            errorMessage.textContent = 'Invalid name format';
+            return false;
+        }
+    } else if (searchCriteria === 'ein') {
+        const einP = /^\d{9}$/;
+        if(!einP.test(searchQuery)){
+            errorMessage.textContent = 'Invalid name EIN format - 9 digits required';
+            return false;
+        }
+    } else if (searchCriteria === 'itin') {
+        const itinP = /^\d{9}$/;
+        if(!itinP.test(searchQuery)){
+            errorMessage.textContent = 'Invalid name ITIN format - 9 digits required';
+            return false;
+        }
+    }else if (searchCriteria === 'ssn') {
+        const ssnP = /^\d{9}$/;
+        if(!ssnP.test(searchQuery)){
+            errorMessage.textContent = 'Invalid name SSN format - 9 digits required';
+            return false;
+        }
     }
-}*/
+    return true;
+}
+
 
