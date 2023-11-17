3b671626259838fcb92f141acb3a824aa4f47a43
Stephen-Admin-at-467073512286 <stephendmiller1991@gmail.com>
2023-11-17 07:15:00 -0500

    cookies

--- src/app.js
+++ src/app.js
@@ -9,16 +9,20 @@ async function main() {
     dotenv.config();
     const app = express();
     app.use(session({
+        name: 'FrapSess',
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
         store: MongoStore.create({
             mongoUrl: process.env.MONGO_SESSION_URL
-          })    
+          }) ,  
+        cookie: {
+            maxAge: 3600000,
+            secure: false
+        }
     }));
     app.use(express.json());
     configRoutesFunction(app);
-
     app.listen(3000, () => {
     console.log('app running on http://localhost:3000');
     });
