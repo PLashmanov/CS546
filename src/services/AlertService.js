cf6c27ae1926b6199a1d80cc2999715ab010e1c2
Stephen-Admin-at-467073512286 <stephendmiller1991@gmail.com>
2023-11-23 12:18:09 -0500

    alert non blocking

--- src/services/AlertService.js
+++ src/services/AlertService.js
@@ -1,63 +1,67 @@
 import { MailSender } from "../util/MailSender.js";
 import { fetchUsersFromIds } from "../db/usersData.js";
 import { ObjectId } from 'mongodb';
-import {isValidEmailAddress, isArray} from "../validations/Validations.js"
-import {getFraudsterById} from "../db/fraudstersData.js"
-
-/**
- * Alert Users About Fraudster Activity
-   @example
-    dotenv.config();
-    const alertService = new AlertService();
-    let fraudsterID = "507f1f77bcf86cd799439011";
-    try{
-        await alertService.alertUsers(fraudsterID);
-    }
-    catch(e){
-        console.error("error sending email for modification of fraduster: " , fraudsterID , "with error " , e)
-    }
-*/
+import { isValidEmailAddress, isArray } from "../validations/Validations.js";
+import { getFraudsterById } from "../db/fraudstersData.js";
 
 class AlertService {
   constructor() {
     this.mailSender = new MailSender();
+    this.emailQueue = [];
+    this.isProcessing = false;
+    this.#initializeEmailWorker();
   }
-  /** 
-    @param {string} fraudsterID - The unique identifier of the fraudster. Needs to be a valid mongo ID
-  **/
   async alertUsers(fraudsterID) {
     if (!ObjectId.isValid(fraudsterID)) {
       throw new Error('Invalid ObjectId passed to alert service:', fraudsterID);
     }
-    const emailAddresses = await this.#fetchEmailsWithAlertEnabled(fraudsterID) 
+    const emailAddresses = await this.#fetchEmailsWithAlertEnabled(fraudsterID);
     if (!isArray(emailAddresses) || emailAddresses.length === 0) {
       console.error('No valid email addresses retrieved for fraudsterID:', fraudsterID);
+      return;
     }
-    for (let userEmail of emailAddresses) {  
+    for (let userEmail of emailAddresses) {
       if (await isValidEmailAddress(userEmail)) {
-         await this.#sendAlertEmail(userEmail, fraudsterID);
-      }
-      else{
-        console.error("invalid email format:  " ,  userEmail)
+        this.emailQueue.push({ userEmail, fraudsterID });
+      } else {
+        console.error("Invalid email format:", userEmail);
       }
     }
   }
+
+  #initializeEmailWorker() {
+    setInterval(() => {
+      if (!this.isProcessing && this.emailQueue.length > 0) {
+        this.isProcessing = true;
+        const task = this.emailQueue.shift(); 
+        this.#processEmail(task)
+          .then(() => this.isProcessing = false)
+          .catch(error => {
+            console.error('Failed to process email task:', error);
+            this.isProcessing = false;
+          });
+      }
+    }, 2000);
+  }
+  async #processEmail(task) {
+    const { userEmail, fraudsterID } = task;
+    const status = await this.mailSender.sendMail({
+      from: process.env.FRAP_EMAIL,
+      to: userEmail,
+      subject: 'Alert from FRAP',
+      body: `A fraudster you reported has been modified. Fraudster ID: ${fraudsterID}`,
+    });
+    console.log(`Email sent: ${status.response}, Accepted: ${status.accepted}`);
+  }
+
   async #fetchEmailsWithAlertEnabled(fraudsterID) {
-      const fraudster = await getFraudsterById(fraudsterID); 
-      if (!fraudster || !fraudster.users) {
-        throw new Error('Fraudster not found or userIds missing in fraudster collection');
+    const fraudster = await getFraudsterById(fraudsterID);
+    if (!fraudster || !fraudster.users) {
+      throw new Error('Fraudster not found or userIds missing in fraudster collection');
     }
-      const users = await fetchUsersFromIds(fraudster.users);
-      return users.filter(user => user.notifications).map(user => user.email);
-  }
-  async #sendAlertEmail(userEmail , fraudsterID) { 
-      const status = await this.mailSender.sendMail({
-        from: process.env.FRAP_EMAIL,
-        to: userEmail,
-        subject: 'Alert from FRAP',
-        body: `A fraudster you reported has been modified. Fraudster ID: ${fraudsterID}`,});
-      console.log(`Email sent: ${status.response}, Accepted: ${status.accepted}`);
+    const users = await fetchUsersFromIds(fraudster.users);
+    return users.filter(user => user.notifications).map(user => user.email);
   }
 }
- 
+
 export { AlertService };
